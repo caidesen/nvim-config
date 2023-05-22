bb028737a9fe731246774ad6ab82f6d74cbb4e8f
蔡德森 <asen001@live.com>
2023-05-22 15:07:27 +0800

    feat: css and html lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,15 +29,100 @@ return {
 			"hrsh7th/nvim-cmp",
 		},
 		config = function()
+			local luasnip = require("luasnip")
 			local cmp = require("cmp")
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				window = {
+					-- completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-c>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif cmp.visible() then
+							cmp.confirm({ select = true })
+						else
+							fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "luasnip", priority = 100 },
+					{ name = "nvim_lsp", priority = 90 },
+					{
+						name = "buffer",
+						priority = 80,
+						max_item_count = 3,
+						keyword_length = 5,
+					},
+					{
+						name = "path",
+						priority = 80,
+						max_item_count = 3,
+						keyword_length = 3,
+					},
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "nvim_lua" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- Set configuration for specific filetype.
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
 		end,
 	},
 	{
 		"folke/neodev.nvim",
 		event = "VeryLazy",
-		config = function() end,
+		config = function()
+			require("neodev").setup()
+		end,
 	},
 	{
 		"jose-elias-alvarez/null-ls.nvim",
