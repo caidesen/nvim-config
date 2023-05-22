dce317fc4db94497771c4f15ab3e9619be85693c
蔡德森 <asen001@live.com>
2023-05-22 10:45:52 +0800

    feat: conflict highlight

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,5 @@
 return {
+	-- show line change status
 	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
@@ -6,9 +7,31 @@ return {
 			require("gitsigns").setup()
 		end,
 	},
+	-- git plugin
 	{
 		"tpope/vim-fugitive",
 		event = "VeryLazy",
 		cmd = "Git",
+	},
+	-- conflict highlight
+	{
+		"rhysd/conflict-marker.vim",
+		event = "VeryLazy",
+		config = function()
+			vim.cmd([[
+			" disable the default highlight group
+let g:conflict_marker_highlight_group = ''
+
+" Include text after begin and end markers
+let g:conflict_marker_begin = '^<<<<<<< .*$'
+let g:conflict_marker_end   = '^>>>>>>> .*$'
+
+highlight ConflictMarkerBegin guibg=#2f7366
+highlight ConflictMarkerOurs guibg=#2e5049
+highlight ConflictMarkerTheirs guibg=#344f69
+highlight ConflictMarkerEnd guibg=#2f628e
+highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+			]])
+		end,
 	},
 }
