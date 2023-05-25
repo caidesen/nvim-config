53c03af80654e22e9647d00d1d8ecd100258bcf7
蔡德森 <asen001@live.com>
2023-05-25 10:10:56 +0800

    feat: add comment plugin

--- lua/plugins/edit-support.lua
+++ lua/plugins/edit-support.lua
@@ -2,7 +2,13 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		config = function()
-			require("nvim-autopairs").setup({})
+			require("nvim-autopairs").setup()
 		end,
 	},
+	{
+		"numToStr/Comment.nvim",
+		config = function ()
+			require("Comment").setup()
+		end
+	}
 }
