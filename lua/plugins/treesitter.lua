1aa75d52406b279f2966dca625caea5f12633d47
蔡德森 <asen001@live.com>
2023-05-28 14:12:34 +0800

    feat: treesitter textobj

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,7 +21,7 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		event = "VeryLazy",
-		dependencies = { "windwp/nvim-ts-autotag" },
+		dependencies = { "windwp/nvim-ts-autotag", "nvim-treesitter/nvim-treesitter-textobjects" },
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = {
@@ -66,6 +66,19 @@ return {
 					-- colors = {}, -- table of hex strings
 					-- termcolors = {} -- table of colour name strings
 				},
+				textobjects = {
+					select = {
+						enable = true,
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
+
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+						},
+					},
+				},
 			})
 			vim.lsp.handlers["textDocument/publishDiagnostics"] =
 				vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -78,20 +91,4 @@ return {
 				})
 		end,
 	},
-	-- {
-	-- 	"windwp/nvim-ts-autotag",
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		require("nvim-ts-autotag").setup()
-	-- 		vim.lsp.handlers["textDocument/publishDiagnostics"] =
-	-- 			vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	-- 				underline = true,
-	-- 				virtual_text = {
-	-- 					spacing = 5,
-	-- 					severity_limit = "Warning",
-	-- 				},
-	-- 				update_in_insert = true,
-	-- 			})
-	-- 	end,
-	-- },
 }
