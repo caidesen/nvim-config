b390a6af5321c6c99004399186743c1165896a5a
蔡德森 <asen001@live.com>
2023-06-06 14:20:11 +0800

    feat: buffer line style change

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -33,9 +33,11 @@ return {
 			local mocha = require("catppuccin.palettes").get_palette("mocha")
 			vim.keymap.set("n", "<Leader>b", ":BufferLinePick<CR>", { noremap = true, silent = true })
 			vim.keymap.set("n", "<Leader>B", ":BufferLinePickClose<CR>", { noremap = true, silent = true })
-			require("bufferline").setup({
+			local bufferline = require("bufferline")
+			bufferline.setup({
 				highlights = require("catppuccin.groups.integrations.bufferline").get({
-					styles = { "italic", "bold" },
+					-- styles = { "italic", "bold" },
+					styles = { "bold" },
 					custom = {
 						mocha = {
 							background = { fg = mocha.text },
@@ -43,20 +45,15 @@ return {
 					},
 				}),
 				options = {
-					numbers = "ordinal",
 					diagnostics = "nvim_lsp",
-					-- diagnostics_indicator = function(count)
-					-- 	return "(" .. count .. ")"
-					-- end,
 					offsets = {
 						{
 							filetype = "NvimTree",
-							text = "🥷 File Explorer",
+							text = [[🌀 File Explorer]],
 						},
 					},
-					show_buffer_icons = true,
-					show_buffer_close_icons = true,
-					show_close_icon = true,
+					show_buffer_icons = false,
+					show_buffer_close_icons = false,
 					show_tab_indicators = true,
 					persist_buffer_sort = true,
 					separator_style = "slant",
