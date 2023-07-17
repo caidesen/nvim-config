f664660fdc440153e9fd23c5dd93cd39177db533
蔡德森 <asen001@live.com>
2023-07-17 17:20:22 +0800

    feat: github copilot

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,21 +1,6 @@
 return {
-	-- {
-	-- 	"Exafunction/codeium.vim",
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		vim.g.codeium_disable_bindings = 1
-	-- 		vim.keymap.set("i", "<C-g>", function()
-	-- 			return vim.fn["codeium#Accept"]()
-	-- 		end, { expr = true })
-	-- 		-- vim.keymap.set("i", "<c-;>", function()
-	-- 		-- 	return vim.fn["codeium#CycleCompletions"](1)
-	-- 		-- end, { expr = true })
-	-- 		-- vim.keymap.set("i", "<c-,>", function()
-	-- 		-- 	return vim.fn["codeium#CycleCompletions"](-1)
-	-- 		-- end, { expr = true })
-	-- 		-- vim.keymap.set("i", "<c-x>", function()
-	-- 		-- 	return vim.fn["codeium#Clear"]()
-	-- 		-- end, { expr = true })
-	-- 	end,
-	-- },
+	{
+		"github/copilot.vim",
+		event = "VeryLazy",
+	},
 }
