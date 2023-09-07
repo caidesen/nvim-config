fd2386f4464d298b84cd22d093de2aae5268375c
蔡德森 <asen001@live.com>
2023-09-07 16:03:38 +0800

    fix: lualine throw git error

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -19,11 +19,21 @@ return {
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
 		config = function()
+			local function diff_source()
+				local gitsigns = vim.b.gitsigns_status_dict
+				if gitsigns then
+					return {
+						added = gitsigns.added,
+						modified = gitsigns.changed,
+						removed = gitsigns.removed,
+					}
+				end
+			end
 			require("lualine").setup({
 				extensions = { "fugitive" },
 				sections = {
 					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_b = { "branch", { "diff", source = diff_source }, "diagnostics" },
 					lualine_c = { "filename" },
 					lualine_x = { "filetype" },
 					lualine_y = { "progress" },
@@ -32,8 +42,8 @@ return {
 				options = {
 					theme = "catppuccin",
 					disabled_filetypes = {
-						statusline = {'NvimTree'},
-						winbar = {'NvimTree'},
+						statusline = { "NvimTree" },
+						winbar = { "NvimTree" },
 					},
 				},
 			})
